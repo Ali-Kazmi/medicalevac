57b89e421e2dbdea6036d81d630d7901f25fb1c3
ali <3ali.kazmi@gmail.com>
2020-07-26 21:07:41 -0700

    initial setup, made some empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OptionsBarComponent } from './components/options-bar/options-bar.component';
+import { TitleBarComponent } from './components/title-bar/title-bar.component';
+import { OutputTableComponent } from './components/output-table/output-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OptionsBarComponent,
+    TitleBarComponent,
+    OutputTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/options-bar/options-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-options-bar',
+  templateUrl: './options-bar.component.html',
+  styleUrls: ['./options-bar.component.css']
+})
+export class OptionsBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
