b934670c016ad23f35795a64704e2540eefa1e05
ali <3ali.kazmi@gmail.com>
2020-07-27 15:23:47 -0700

    added in around 10 more of the landing zones data

--- src/app/LandingZoneData.ts
+++ src/app/LandingZoneData.ts
@@ -14,5 +14,85 @@ export const LandingZones = [
         slopePitch:null,
         lat:34.769100,
         long:-84.017650
+    },
+    {
+        name: 'Deerlick',
+        elevation:3150,
+        slopeRoll: 5, 
+        slopePitch:-3,
+        lat:34.642800,
+        long:-84.160267
+    },
+    {
+        name: 'Donovan',
+        elevation:1850,
+        slopeRoll: -10, 
+        slopePitch:0,
+        lat:34.592983,
+        long:-84.123367
+    },
+    {
+        name: 'Edmonson',
+        elevation:2578,
+        slopeRoll: 11, 
+        slopePitch:7,
+        lat:34.678733,
+        long:-84.128583
+    },
+    {
+        name: 'Finney',
+        elevation:2900,
+        slopeRoll: 2.3, 
+        slopePitch:-2,
+        lat:34.778300,
+        long:-84.032717
+    },
+    {
+        name: 'Flores',
+        elevation:2650,
+        slopeRoll: -8, 
+        slopePitch:1,
+        lat:34.780283,
+        long:-84.118950
+    },
+    {
+        name: 'Frying Pan',
+        elevation:3200,
+        slopeRoll: 0, 
+        slopePitch:0,
+        lat:34.649083,
+        long:-84.155567
+    },
+    {
+        name: 'Garrett Farms',
+        elevation:2325,
+        slopeRoll: 0, 
+        slopePitch:0,
+        lat:34.686483,
+        long:-84.052483
+    },
+    {
+        name: 'Gold',
+        elevation:3225,
+        slopeRoll: 10, 
+        slopePitch:1,
+        lat:34.677050,
+        long:-84.175567
+    },
+    {
+        name: 'Hawk Mountain',
+        elevation:3425,
+        slopeRoll: 5, 
+        slopePitch:-5,
+        lat:34.660583,
+        long:-84.138383
+    },
+    {
+        name: 'Johnson',
+        elevation:3040,
+        slopeRoll: -3, 
+        slopePitch:8,
+        lat:34.664600,
+        long:-84.163767
     }
 ];
