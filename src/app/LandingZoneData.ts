52ec217eb14505bca9df36e3a39ace0caf4b2b74
ali <3ali.kazmi@gmail.com>
2020-07-26 22:02:45 -0700

    added some sample lat long data

---
+++ src/app/LandingZoneData.ts
@@ -0,0 +1,18 @@
+export const LandingZones = [
+    {
+        name: 'Big Stamp',
+        elevation:3175,
+        slopeRoll: 9, 
+        slopePitch:5,
+        lat:34.747333,
+        long:-84.071333
+    },
+    {
+        name: 'Chestnut',
+        elevation:3000,
+        slopeRoll: null, 
+        slopePitch:null,
+        lat:34.769100,
+        long:-84.017650
+    }
+];
