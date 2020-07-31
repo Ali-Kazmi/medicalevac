97c76db8ed373d21e40bf27fa83dc0cd96314d35
ali <3ali.kazmi@gmail.com>
2020-07-30 18:15:45 -0700

    Added option to hide and show options bar

--- src/app/components/output-table/output-table.component.ts
+++ src/app/components/output-table/output-table.component.ts
@@ -2,16 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import {LandingZones} from "../../LandingZoneData";
 
 @Component({
-  selector: 'app-output-table',
+  selector: 'app-output-table', 
   templateUrl: './output-table.component.html',
   styleUrls: ['./output-table.component.css']
 })
 export class OutputTableComponent implements OnInit {
   headers;
   lz;
+  isCollapsed : boolean=true; 
   constructor() { 
     this.headers = ["id", "name", "elevation", "slopeRoll", "slopePitch","lat","long"];
     this.lz=LandingZones;
+  }
+
+  toggleCollapse(){
+    this.isCollapsed=!this.isCollapsed; 
   }
 
   ngOnInit(): void {
