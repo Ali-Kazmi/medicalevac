5e5f068aafec0ab30cb2b81daad4288b7f3b35df
ali <3ali.kazmi@gmail.com>
2020-08-09 22:27:53 -0700

    Added map on showcomponent (with christophers help

--- src/app/components/output-table/output-table.component.ts
+++ src/app/components/output-table/output-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {LandingZones} from "../../LandingZoneData";
+declare let L;
 
 @Component({
   selector: 'app-output-table', 
@@ -20,6 +21,18 @@ export class OutputTableComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const map = L.map('map').setView([34, -84], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    for (var x = 0; x < this.lz.length; x++) {
+      //console.log("landing zone: "+ this.lz[x].long)
+      L.marker([this.lz[x].lat, this.lz[x].long]).addTo(map)
+        .bindPopup(this.lz[x].name)
+        .openPopup();
+    }
   }
 
 }
